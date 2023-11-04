bddb82b132c75d1328573b04ab6c3c980551fc25
ShowkatJamil <showkatjamil2012@gmail.com>
2023-11-04 23:53:57 +0600

    new update-1

--- showPersonInfo.js
+++ showPersonInfo.js
@@ -1,54 +1,13 @@
 function showPersonInfo() {
   var person = document.getElementById("nameBox").value;
-  var mark = parseInt(document.getElementById("markBox").value);
+  var email = document.getElementById("emailBox").value;
   var phone = document.getElementById("phoneBox").value;
 
-  if (mark >= 90) {
-    document.getElementById("content").innerHTML = `
-      
-      <h4>Hello ${person}, your Grade is A`;
-  } else if (mark >= 85 && mark < 90) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is A-`;
-  } else if (mark >= 80 && mark < 85) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is B+`;
-  } else if (mark >= 75 && mark < 80) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is B`;
-  } else if (mark >= 70 && mark < 75) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is B-`;
-  } else if (mark >= 65 && mark < 70) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is C+`;
-  } else if (mark >= 60 && mark < 65) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is C`;
-  } else if (mark >= 55 && mark < 60) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is C-`;
-  } else if (mark >= 50 && mark < 55) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is D+`;
-  } else if (mark >= 45 && mark < 50) {
-    document.getElementById("content").innerHTML = `
-        
-        <h4>Hello ${person}, your Grade is D`;
-  } else {
-    document.getElementById("content").innerHTML = `
-      <h4> Sorry ${person}, You got F </h4>`;
-  }
 
-  document.getElementById("content").innerHTML = `
-  <h4> Sorry ${phone}, You got F </h4>`;
-
+  
+  document.getElementById("showBox").innerHTML = `
+  Mr./Mrs ${person}, <br>
+  Your email is: ${email}  <br>
+  Your Phone Number is: ${phone}
+  `;
 }
